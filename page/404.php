cb350e6f642c8ab605cd1952a2de36b2400c445e
Алексей <aleksreut@yandex.ru>
2020-04-30 18:14:09 +0500

    page 404

---
+++ page/404.php
@@ -0,0 +1,17 @@
+<section class="error_page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md text-center ">
+                    <h1 class="mb-3">Страница не найдена</h1>
+                    <h4 class="mb-3">или не существует</h4>
+                <div class="col- mb-5">
+                    <span>Воспользуйтесь поиском или перейдите на главную страницу сайта</span>
+                </div>
+                <div class="col- pt-4">
+                    <span class="mdi mdi-magnify search-icon"></span>
+                    <input name="search" type="text" class="search col-sm-10">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>  
---
+++ page/feedback.php
@@ -0,0 +1,14 @@
+<section class="feedback">
+    <div class="container">
+        <div class="row">
+            <div class="col-">
+              <h2>Контакты</h2>  
+                <p>Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="col-"></div>
+        </div>
+    </div>
+</section>
