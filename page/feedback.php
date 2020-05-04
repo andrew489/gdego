399cf56285754648c76bc0142aff5a9ef6da0bcd
Алексей <aleksreut@yandex.ru>
2020-05-04 20:24:35 +0500

    page about us

--- page/feedback.php
+++ page/feedback.php
@@ -1,14 +1,46 @@
 <section class="feedback">
     <div class="container">
         <div class="row">
-            <div class="col-">
-              <h2>Контакты</h2>  
-                <p>Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
-                <span></span>
-                <span></span>
-                <span></span>
+            <div class="col-12 col-sm-6 pt-4">
+              <h2 class="mt-5">Контакты</h2>  
+                <p class="mb-4">Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
+                <span class="email">
+                    <span class="mdi mdi-email-open email-icon brown-color"></span>
+                    example@gdego.ru
+                </span>
+                <span class="phone">
+                    <span class="mdi mdi-cellphone-iphone phone-icon brown-color"></span>
+                        +7 (225) 555-0118
+                </span>
+                <span class="adress">
+                    <span class="mdi mdi-map email-icon brown-color"></span>
+                    6386 Spring St undefined Anchorage, Georgia 12473 United States
+                </span>
+                <form>
+                    <div class="form-row col-12 pl-0 pr-0 ml-0 mb-2">
+                        <div class="form-group col-6 pl-0 pr-3">
+                            <span class="mb-1">Имя</span>
+                            <input type="text" class="form-control" >
+                        </div>
+                        <div class="form-group col-6 pr-0 pl-3">
+                            <span class="mb-1">Email</span>
+                            <input type="text" class="form-control" id="inputEmail4">
+                        </div>
+                    </div>
+                        <span class="mb-1">Сообщение</span>
+                        <div class="form-group col-12 pl-0">
+                        <textarea name="" id="" cols="65" rows="7"></textarea>
+                    </div>
+                        <button type="submit" class="btn mb-2 mt-2">Отправить</button>
+                    
+                </form>
             </div>
-            <div class="col-"></div>
+            <div class="col-12 col-sm-6 pt-4">
+                <div class="map-pannel mt-5">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d17474.23191279465!2d60.609323885234!3d56.80684376871794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1587905649302!5m2!1sru!2sru"
+                        width="540" height="632" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+            </div>
         </div>
     </div>
 </section>
