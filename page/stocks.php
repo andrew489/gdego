eeb3ebea8ee0771fa1cf075a89d84501c8b5f508
Алексей <aleksreut@yandex.ru>
2020-05-05 22:41:14 +0500

    page stocks

---
+++ page/stocks.php
@@ -0,0 +1,244 @@
+<section class="stocks">
+    <div class="container">
+        <h2>Акции и скидки</h2>
+        <p class="mb-4">Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
+        <div class="d-flex justify-content-center mb-3 pb-3" >
+        <a href="#" class="banner "><img src="../img/stocks_banner.png" alt="banner"></a>
+        </div>
+
+        <div class="row d-flex align-items-center">
+            <div class="col-auto pl-0">
+                <img src="../img/stocks_img1.png" alt="">
+            </div>
+            <div class="col-9 pt-2 pb-3">
+                <div class="row">
+                    <div class="col-10 pl-0">
+                        <a href="#" class="brown-color">
+                            <span class="mdi mdi-bookmark-outline mdi-24px "></span>
+                        </a> 
+                        <h3>Title</h3>
+                    </div>
+                   
+                    <div class="col-2 raiting text-right pr-0 ">
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                    </div>
+                </div> 
+                    <p class="mt-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare vulputate sed imperdiet nec purus. Ullamcorper tortor nisi, sed viverra pulvinar feugiat ante. Bibendum cum erat libero aliquet ipsum porta in amet, feugiat. Mattis placerat integer donec pharetra dictum. Egestas senectus eget vitae, id viverra purus rhoncus. Quis viverra dui magna in ut.</p>
+               
+            </div>
+        </div>
+
+        <div class="row d-flex align-items-center">
+            <div class="col-auto pl-0">
+                <img src="../img/stocks_img2.png" alt="">
+            </div>
+            <div class="col-9 pt-2 pb-3">
+                <div class="row">
+                    <div class="col-10 pl-0">
+                        <a href="#" class="brown-color">
+                            <span class="mdi mdi-bookmark-outline mdi-24px"></span>
+                        </a> 
+                        <h3>Title</h3>
+                    </div>
+                   
+                    <div class="col-2 raiting text-right pr-0">
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                    </div>
+                </div> 
+                    <p class="mt-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare vulputate sed imperdiet nec purus. Ullamcorper tortor nisi, sed viverra pulvinar feugiat ante. Bibendum cum erat libero aliquet ipsum porta in amet, feugiat. Mattis placerat integer donec pharetra dictum. Egestas senectus eget vitae, id viverra purus rhoncus. Quis viverra dui magna in ut.</p>
+               
+            </div>
+        </div>
+
+        <div class="row d-flex align-items-center">
+            <div class="col-auto pl-0">
+                <img src="../img/stocks_img3.png" alt="">
+            </div>
+            <div class="col-9 pt-2 pb-3">
+                <div class="row">
+                    <div class="col-10 pl-0">
+                        <a href="#" class="brown-color">
+                            <span class="mdi mdi-bookmark-outline mdi-24px"></span>
+                        </a> 
+                        <h3>Сеть ресторанов и закусочных "Марабунта"</h3>
+                    </div>
+                   
+                    <div class="col-2 raiting text-right pr-0">
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                    </div>
+                </div> 
+                    <p class="mt-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare vulputate sed imperdiet nec purus. Ullamcorper tortor nisi, sed viverra pulvinar feugiat ante. Bibendum cum erat libero aliquet ipsum porta in amet, feugiat. Mattis placerat integer donec pharetra dictum. Egestas senectus eget vitae, id viverra purus rhoncus. Quis viverra dui magna in ut.</p>
+               
+            </div>
+        </div>
+
+        <div class="row d-flex align-items-center">
+            <div class="col-auto pl-0">
+                <img src="../img/stocks_img4.png" alt="">
+            </div>
+            <div class="col-9 pt-2 pb-3">
+                <div class="row">
+                    <div class="col-10 pl-0">
+                        <a href="#" class="brown-color">
+                            <span class="mdi mdi-bookmark-outline mdi-24px"></span>
+                        </a> 
+                        <h3>Title</h3>
+                    </div>
+                   
+                    <div class="col-2 raiting text-right pr-0">
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                    </div>
+                </div> 
+                    <p class="mt-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare vulputate sed imperdiet nec purus. Ullamcorper tortor nisi, sed viverra pulvinar feugiat ante. Bibendum cum erat libero aliquet ipsum porta in amet, feugiat. Mattis placerat integer donec pharetra dictum. Egestas senectus eget vitae, id viverra purus rhoncus. Quis viverra dui magna in ut.</p>
+               
+            </div>
+        </div>
+
+        <div class="row d-flex align-items-center">
+            <div class="col-auto pl-0">
+                <img src="../img/stocks_img5.png" alt="">
+            </div>
+            <div class="col-9 pt-2 pb-3">
+                <div class="row">
+                    <div class="col-10 pl-0">
+                        <a href="#" class="brown-color">
+                            <span class="mdi mdi-bookmark-outline mdi-24px"></span>
+                        </a> 
+                        <h3>Title</h3>
+                    </div>
+                   
+                    <div class="col-2 raiting text-right pr-0">
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                    </div>
+                </div> 
+                    <p class="mt-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare vulputate sed imperdiet nec purus. Ullamcorper tortor nisi, sed viverra pulvinar feugiat ante. Bibendum cum erat libero aliquet ipsum porta in amet, feugiat. Mattis placerat integer donec pharetra dictum. Egestas senectus eget vitae, id viverra purus rhoncus. Quis viverra dui magna in ut.</p>
+               
+            </div>
+        </div>
+
+        <div class="row d-flex align-items-center">
+            <div class="col-auto pl-0">
+                <img src="../img/stocks_img6.png" alt="">
+            </div>
+            <div class="col-9 pt-2 pb-3">
+                <div class="row">
+                    <div class="col-10 pl-0">
+                        <a href="#" class="brown-color">
+                            <span class="mdi mdi-bookmark-outline mdi-24px"></span>
+                        </a> 
+                        <h3>Title</h3>
+                    </div>
+                   
+                    <div class="col-2 raiting text-right pr-0">
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                    </div>
+                </div> 
+                    <p class="mt-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare vulputate sed imperdiet nec purus. Ullamcorper tortor nisi, sed viverra pulvinar feugiat ante. Bibendum cum erat libero aliquet ipsum porta in amet, feugiat. Mattis placerat integer donec pharetra dictum. Egestas senectus eget vitae, id viverra purus rhoncus. Quis viverra dui magna in ut.</p>
+               
+            </div>
+        </div>
+
+        <div class="row d-flex align-items-center">
+            <div class="col-auto pl-0">
+                <img src="../img/stocks_img7.png" alt="">
+            </div>
+            <div class="col-9 pt-2 pb-3">
+                <div class="row">
+                    <div class="col-10 pl-0">
+                        <a href="#" class="brown-color">
+                            <span class="mdi mdi-bookmark-outline mdi-24px"></span>
+                        </a> 
+                        <h3>Сеть ресторанов и закусочных "Марабунта"</h3>
+                    </div>
+                   
+                    <div class="col-2 raiting text-right pr-0">
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                    </div>
+                </div> 
+                    <p class="mt-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare vulputate sed imperdiet nec purus. Ullamcorper tortor nisi, sed viverra pulvinar feugiat ante. Bibendum cum erat libero aliquet ipsum porta in amet, feugiat. Mattis placerat integer donec pharetra dictum. Egestas senectus eget vitae, id viverra purus rhoncus. Quis viverra dui magna in ut.</p>
+               
+            </div>
+        </div>
+
+        <div class="row d-flex align-items-center">
+            <div class="col-auto pl-0">
+                <img src="../img/stocks_img8.png" alt="">
+            </div>
+            <div class="col-9 pt-2 pb-3">
+                <div class="row">
+                    <div class="col-10 pl-0">
+                        <a href="#" class="brown-color">
+                            <span class="mdi mdi-bookmark-outline mdi-24px"></span>
+                        </a> 
+                        <h3>Title</h3>
+                    </div>
+                   
+                    <div class="col-2 raiting text-right pr-0">
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                    </div>
+                </div> 
+                    <p class="mt-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare vulputate sed imperdiet nec purus. Ullamcorper tortor nisi, sed viverra pulvinar feugiat ante. Bibendum cum erat libero aliquet ipsum porta in amet, feugiat. Mattis placerat integer donec pharetra dictum. Egestas senectus eget vitae, id viverra purus rhoncus. Quis viverra dui magna in ut.</p>
+               
+            </div>
+        </div>
+
+        <div class="row d-flex align-items-center">
+            <div class="col-auto pl-0">
+                <img src="../img/stocks_img9.png" alt="">
+            </div>
+            <div class="col-9 pt-2 pb-3">
+                <div class="row">
+                    <div class="col-10 pl-0">
+                        <a href="#" class="brown-color">
+                            <span class="mdi mdi-bookmark-outline mdi-24px"></span>
+                        </a> 
+                        <h3>Title</h3>
+                    </div>
+                   
+                    <div class="col-2 raiting text-right pr-0">
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                        <span class="mdi mdi-star-outline"></span>
+                    </div>
+                </div> 
+                    <p class="mt-4 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare vulputate sed imperdiet nec purus. Ullamcorper tortor nisi, sed viverra pulvinar feugiat ante. Bibendum cum erat libero aliquet ipsum porta in amet, feugiat. Mattis placerat integer donec pharetra dictum. Egestas senectus eget vitae, id viverra purus rhoncus. Quis viverra dui magna in ut.</p>
+               
+            </div>
+        </div>
+
+    </div> <!-- container -->
+</section>
