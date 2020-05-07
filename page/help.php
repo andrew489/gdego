b5eae5df4efb81b8cd08fa668ed48aec202d0cf3
Алексей <aleksreut@yandex.ru>
2020-05-08 03:40:43 +0500

    page search

--- page/help.php
+++ page/help.php
@@ -5,98 +5,98 @@
         <h3>Headline 3</h3>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question1"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question1" class="collapse help_dropdown">
+            <div id="help_question1" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question2"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question2" class="collapse help_dropdown">
+            <div id="help_question2" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question3"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question3" class="collapse help_dropdown">
+            <div id="help_question3" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question4"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question4" class="collapse help_dropdown">
+            <div id="help_question4" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question5"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question5" class="collapse help_dropdown">
+            <div id="help_question5" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question6"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question6" class="collapse help_dropdown">
+            <div id="help_question6" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question7"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question7" class="collapse help_dropdown">
+            <div id="help_question7" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <h3 class="mt-4 pt-3">Headline 3</h3>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question8"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question8" class="collapse help_dropdown">
+            <div id="help_question8" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question9"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question9" class="collapse help_dropdown">
+            <div id="help_question9" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question10"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question10" class="collapse help_dropdown">
+            <div id="help_question10" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question11"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question11" class="collapse help_dropdown">
+            <div id="help_question11" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question12"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question12" class="collapse help_dropdown">
+            <div id="help_question12" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question13"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question13" class="collapse help_dropdown">
+            <div id="help_question13" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question14"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question14" class="collapse help_dropdown">
+            <div id="help_question14" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question15"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question15" class="collapse help_dropdown">
+            <div id="help_question15" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
         <div class="col-12 pr-0 pl-0 mb-3">
             <button class="btn" data-toggle="collapse" data-target="#help_question16"><h4>I Need Help</h4><span class="mdi mdi-chevron-down"></span></button>
-            <div id="help_question16" class="collapse help_dropdown">
+            <div id="help_question16" class="collapse help_dropdown text-60">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet nulla proin tempor senectus commodo tortor. Id diam mauris condimentum fringilla aliquet integer amet nunc. Rutrum cras ultricies ut ornare sed dolor erat. Aliquam id aliquam euismod nulla. Dolor scelerisque mi placerat velit, proin dolor pellentesque malesuada sit. Egestas feugiat tortor maecenas vitae odio commodo duis.
             </div>
         </div>
--- page/search.php
+++ page/search.php
@@ -1,12 +1,78 @@
-<section class="stocks">
-    <div class="container">
-        <h2>Акции и скидки</h2>
-        <p class="mb-sm-4 pb-3">Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
-        <div class="d-flex justify-content-center mb-sm-3 pb-sm-3 mb-0 pb-0" >
-        <a href="#" class="banner d-none d-sm-block"><img src="../img/stocks_banner.png" alt="banner"></a>
+<section class="search pt-5">
+    <div class="container pt-4">
+        <h2 class="d-none d-sm-block">Результаты поиска</h2>
+        <h3 class="d-block d-sm-none mt-3" >Результаты поиска</h3>
+        <p class="mb-4">Поиск по {{ запрос / тег }} {{ item.search }}  pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
+        <div class="col-12 mb-3 pl-0 pr-0 d-none d-sm-block">
+            <div class="row pr-0 pl-0">
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag first name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag first name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag Full name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag Full name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill" role="button"><a href="#"><span>Tag name</span></a></div>
+            </div>
+        </div>
+        <div class="col-10 mb-3 pr-0 pl-0 d-none d-sm-block">
+            <div class="row pr-0 pl-0">
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill" role="button"><a href="#"><span>Tag name</span></a></div>
+            </div>
         </div>
 
-        <div class="row shadow bg-white d-flex align-items-center mb-sm-4 stocks_mb pr-sm-3 pl-sm-3 pl-0 pr-0 ">
+        <div class="col-12 mb-3 pr-0 pl-0 pb-sm-3 d-none d-sm-block">
+            <div class="row pr-0">
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div>
+                <div class="col btn rounded-pill" role="button"><a href="#"><span>Tag name</span></a></div>
+            </div>
+        </div>
+
+        <div class="row d-block d-sm-none mb-4">
+            <ul id="lightSlider">
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag first name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag first name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag Full name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag Full name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+                <li><div class="col btn rounded-pill mr-3" role="button"><a href="#"><span>Tag name</span></a></div></li>
+            </ul>
+        </div>
+
+        <div class="row shadow bg-white d-flex align-items-center mb-sm-4 stocks_mb pr-sm-3 pl-sm-3 pl-0 pr-0 mt-sm-3">
             <div class="col-sm-3 col-12 pl-0 pr-sm-3 pr-0">
                 <img src="../img/stocks_img1.png" alt="">
             </div>
@@ -241,3 +307,60 @@
         </div>
     </div> <!-- container -->
 </section>
+<link type="text/css" rel="stylesheet" href="css/lightslider.css" />                  
+<script src="js/lightslider.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $("#lightSlider").lightSlider({
+            item: 3,
+            autoWidth: true,
+            slideMove: 2, // slidemove will be 1 if loop is true
+            slideMargin: 10,
+     
+            addClass: '',
+            mode: "slide",
+            useCSS: true,
+            cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+            easing: 'linear', //'for jquery animation',////
+     
+            speed: 400, //ms'
+            auto: false,
+            loop: false,
+            slideEndAnimation: true,
+            pause: 2000,
+     
+            keyPress: false,
+            controls: false,
+            prevHtml: '',
+            nextHtml: '',
+     
+            rtl:false,
+            adaptiveHeight:false,
+     
+            vertical:false,
+            verticalHeight:500,
+            vThumbWidth:100,
+     
+            thumbItem:10,
+            pager: false,
+            gallery: false,
+            galleryMargin: 5,
+            thumbMargin: 5,
+            currentPagerPosition: 'middle',
+     
+            enableTouch:true,
+            enableDrag:true,
+            freeMove:true,
+            swipeThreshold: 40,
+     
+            responsive : [],
+     
+            onBeforeStart: function (el) {},
+            onSliderLoad: function (el) {},
+            onBeforeSlide: function (el) {},
+            onAfterSlide: function (el) {},
+            onBeforeNextSlide: function (el) {},
+            onBeforePrevSlide: function (el) {}
+        });
+    });
+    </script>
