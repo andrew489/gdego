221d5b6ba445bde5cf871756f99ba5b99430e496
Алексей <aleksreut@yandex.ru>
2020-05-07 21:57:57 +0500

    page help

--- page/404.php
+++ page/404.php
@@ -1,13 +1,13 @@
 <section class="error_page">
     <div class="container">
         <div class="row">
-            <div class="col-md text-center ">
-                    <h1 class="mb-3">Страница не найдена</h1>
+            <div class="col md text-center ">
+                    <h1>Страница не найдена</h1>
                     <h4 class="mb-3">или не существует</h4>
-                <div class="col- mb-5">
+                <div class="col mb-5">
                     <span>Воспользуйтесь поиском или перейдите на главную страницу сайта</span>
                 </div>
-                <div class="col- pt-4">
+                <div class="form col-12 pt-4">
                     <span class="mdi mdi-magnify search-icon"></span>
                     <input name="search" type="text" class="search col-sm-10">
                 </div>
