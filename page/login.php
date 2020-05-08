2fdad21b996743158cf5e82bd99b106c3a50bbe4
Алексей <aleksreut@yandex.ru>
2020-05-08 18:59:16 +0500

    pade login

---
+++ page/login.php
@@ -0,0 +1,32 @@
+<section class="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 ">
+                <h2 class="mb-3 pb-3">Войти на сайт</h2>
+                <p class="mb-3 pb-3">Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
+                <a href="#" class="btn shadow text-white brown--default mr-4"> <span class="mdi mdi-google"></span> Войти с помощью google</a>
+                <a href="#" class="btn shadow mr-4"><span class="mdi mdi-twitter brown-color"></span></a>
+                <a href="#" class="btn shadow"><span class="mdi mdi-facebook brown-color"></span> </a>
+                <small class="form-text text-muted mb-3 pb-3 pt-3 mt-3 text-center">или</small>
+                <form>
+                    <div class="form-group">
+                        <span for="exampleInputEmail1" class="mb-2">Email</span>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
+                    </div>
+                    <div class="form-group">
+                        <span for="exampleInputPassword1" class="mb-2">Пароль</span>
+                        <input type="password" class="form-control" id="exampleInputPassword1" >
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input brown--default" id="exampleCheck1">
+                        <span class="form-check-label text-60" for="exampleCheck1">Я принимаю условия <a href="#">политики конфиденциальности.</a></span>
+                    </div>
+                    <button type="submit" class="btn shadow text-white brown--default">Войти</button>
+                    <a href="#" class="brown-color">Забыли пароль?</a>
+                </form>
+            </div><!--col-sm-6-->
+
+            <div class="col-sm-6"></div>
+        </div> <!--row--> 
+    </div> <!--container-->
+</section>
