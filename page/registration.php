7466f9030a42695e5b385fd8fca9387d0a873596
Алексей <aleksreut@yandex.ru>
2020-05-08 17:36:27 +0500

    page registration

---
+++ page/registration.php
@@ -0,0 +1,30 @@
+<section class="registration">
+    <div class="container">
+        <h2 class="mb-3 pb-3">Регистрация</h2>
+        <p class="col-6 mb-3 pb-3 pl-0" >Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
+        <div class="row d-flex justify-content-center pt-3">
+            <div class="col-sm-5 col-12  mt-4 mr-0 mr-sm-3 pt-3">
+                <div class="bg-white  registration_card">
+                <h2 class="mb-3 pb-3 mb-3">Организация</h2>
+               
+                <p class="mb-4 pb-3">Anim sit dolor irure eiusmod nostrud ut est ullamco est. Consequat Lorem ipsum exercitation tempor eiusmod amet culpa esse nisi non tempor enim.</p>
+               
+                <a href="#" class="btn text-white shadow "><span class="mdi mdi-arrow-left pr-3"></span>Назад</a>
+                <a href="#" class="btn text-white float-right shadow right-button">Далее <span class="mdi mdi-arrow-right pl-3"></span></a>
+                
+                
+                </div>
+            </div>
+
+
+            <div class=" col-sm-5 col-12 mt-4 ml-sm-3 ml-0 pt-3 pl-sm-3">
+                <div class="registration_card bg-white">
+                <h2 class="mb-3 pb-3">Пользователь</h2>
+                <p class="mb-4 pb-3">Anim sit dolor irure eiusmod nostrud ut est ullamco est. Consequat Lorem ipsum exercitation tempor eiusmod amet culpa esse nisi non tempor enim.</p>
+                <a href="#" class="btn text-white shadow "><span class="mdi mdi-arrow-left pr-3"></span>Назад</a>
+                <a href="#" class="btn text-white float-right shadow right-button">Далее<span class="mdi mdi-arrow-right pl-3"></span></a>
+                </div>
+            </div>
+        </div> <!--row-->
+    </div> <!--container-->
+</section>
