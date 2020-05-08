74b03bb059a9f314a4c6f11b868ec804aa23edcd
Алексей <aleksreut@yandex.ru>
2020-05-09 03:00:44 +0500

    page login

--- page/login.php
+++ page/login.php
@@ -1,27 +1,30 @@
 <section class="login_site">
     <div class="container">
         <div class="row">
-            <div class="col-sm-6 ">
+            <div class="col-sm-6 col-12">
                 <h2 class="mb-3 pb-3">Войти на сайт</h2>
                 <p class="mb-3 pb-3">Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
-                <a href="#" class="btn shadow text-white brown--default mr-4"> <span class="mdi mdi-google"></span> Войти с помощью google</a>
-                <a href="#" class="btn shadow mr-4 bg-light"><span class="mdi mdi-twitter brown-color"></span></a>
-                <a href="#" class="btn shadow bg-light"><span class="mdi mdi-facebook brown-color"></span> </a>
+                <a href="#" class="btn shadow text-white brown--default mr-4"> <span class="mdi mdi-google mdi-24px"></span> Войти с помощью google</a>
+                <a href="#" class="btn shadow mr-4 bg-light"><span class="mdi mdi-twitter mdi-24px brown-color"></span></a>
+                <a href="#" class="btn shadow bg-light"><span class="mdi mdi-facebook mdi-24px brown-color"></span> </a>
                 
                 <small class="form-text text-muted mb-3 pb-3 pt-3 mt-3 text-center">или</small>
                 
                 <form>
                     <div class="form-group">
                         <span for="exampleInputEmail1" class="mb-2 d-flex">Email</span>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ><span class="mdi mdi-eye"></span>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ><span class="mdi mdi-eye mdi-24px brown-color"></span>
                     </div>
                     <div class="form-group">
                         <span for="exampleInputPassword1" class="mb-2 d-flex">Пароль</span>
-                        <input type="password" class="form-control" id="exampleInputPassword1" ><span class="mdi mdi-eye"></span>
+                        <input type="password" class="form-control" id="exampleInputPassword1" ><span class="mdi mdi-eye mdi-24px brown-color"></span>
                     </div>
-                    <div class="form-check">
-                        <input type="checkbox" name="agree" class="form-check-input brown--default" id="agree" valio="1">
-                        <label for="agree" type="checkbox" ><span class="form-check-label text-60 mb-4 d-flex" for="exampleCheck1">Я принимаю условия <a href="#" class="brown-color align-middle">&nbsp;<ins>политики конфиденциальности.</ins></a></span></label>
+                    <div class="form-check pl-0">
+                        <input type="checkbox" name="agreement" class="form-check-input" id="agreement" valio="1">
+                        <label for="agreement" type="checkbox" >
+                            <span class="form-check-label mb-4 d-flex" >Я принимаю условия <a href="#" class="brown-color align-middle">&nbsp;<ins>политики конфиденциальности.</ins></a>
+                            </span>
+                        </label>
                         
                     </div>
                     <button type="submit" class="btn shadow text-white brown--default">Войти</button>
@@ -29,7 +32,10 @@
                 </form>
             </div><!--col-sm-6-->
 
-            <div class="col-sm-6"></div>
+            <div class="col-sm-6 col-12">
+                <div class="bg_login_site"></div>
+                
+            </div>
         </div> <!--row--> 
     </div> <!--container-->
 </section>
