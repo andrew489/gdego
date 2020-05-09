c7f17c092c32952b12b7dde7f68e5f463bce4c9d
Алексей <aleksreut@yandex.ru>
2020-05-09 15:43:41 +0500

    page registration_user

---
+++ page/registration_comp2.php
@@ -0,0 +1,34 @@
+<section class="registration_comp">
+    <div class="container">
+        <h2 class="mb-3 pb-3">Регистрация</h2>
+        <p class="mb-3 pb-3">Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
+        <div class="row justify-content-center pt-3 mt-4">
+            <div class="col-sm-10 pl-sm-3 pr-sm-3 pr-0 pl-0 bg-white ">
+                <form>
+                    <div class="form-group col-sm-7 col-12 pl-0">
+                        <span for="text_1" class="mb-2 d-flex">text</span>
+                        <input type="text" class="form-control" name="text_1" id="exampleInputEmail1"  >
+                    </div>
+                    <div class="form-group col-sm-7 col-12 pl-0">
+                        <div class="form-group col-6 d-inline-block mr-sm-3 mr-0 pr-0 pl-0">
+                            <span for="text_2" class="mb-2 d-inline-block">text</span>
+                            <input type="text" class="form-control" name="text_2" id="exampleInputEmail1"  >
+                        </div>
+                        <div class="form-group col-5 d-inline-block ml-3 pr-0 pl-0">
+                            <span for="text_3" class="mb-2 d-inline-block ">text</span>
+                            <input type="text" class="form-control" name="text_3" id="exampleInputEmail1"  >
+                        </div>
+                    </div>
+                    <div class="form-group col-sm-7 col-12 mb-5 pl-0">
+                        <span for="text_4" class="mb-2 d-flex">text</span>
+                        <input type="text" class="form-control" name="text_4" id="exampleInputEmail1"  >
+                    </div>
+                    <a href="#" class="btn text-white shadow "><span class="mdi mdi-arrow-left pr-3"></span>Назад</a>
+                    <a href="#" class="btn text-white float-right shadow ">Далее<span class="mdi mdi-arrow-right pl-3"></span></a>
+                </form>
+            </div>
+        
+        </div> <!--row-->
+        
+    </div> <!--container-->
+</section>
---
+++ page/registration_user.php
@@ -0,0 +1,50 @@
+<section class="registration_user">
+    <div class="container">
+        <h2 class="mb-3 pb-3">Регистрация</h2>
+        <p class="mb-3 pb-3">Pariatur ex ipsum sit cillum eiusmod exercitation exercitation velit deserunt excepteur veniam nostrud.</p>
+        <div class="row justify-content-center pt-3 mt-4">
+            <div class="col-sm-10 p-3 pl-sm-3 pr-sm-3 pr-0 pl-0 bg-white shadow">
+                <form class="m-3">
+                    <div class="form-group row pl-3 pr-3">
+                        <div class="form-group col-6 d-inline-block pr-3 pl-0">
+                            <span for="name" class="mb-2 d-inline-block">Имя</span>
+                            <input type="text" class="form-control" name="name" id="name"  >
+                        </div>
+                        <div class="form-group col-6 d-inline-block pr-0 pl-3">
+                            <span for="surnume" class="mb-2 d-inline-block ">Фамилия</span>
+                            <input type="text" class="form-control" name="surnume" id="surnume"  >
+                        </div>
+                    </div>
+                    <div class="form-group row pl-3 pr-3">
+                        <div class="form-group col-6 d-inline-block pr-3 pl-0">
+                            <span for="phone" class="mb-2 d-inline-block">Телефон</span>
+                            <input type="text" class="form-control" name="phone" id="phone"  >
+                        </div>
+                        <div class="form-group col-6 d-inline-block pr-0 pl-3">
+                            <span for="Email" class="mb-2 d-inline-block ">Email</span>
+                            <input type="text" class="form-control" name="Email" id="Email"  >
+                        </div>
+                    </div>
+                    <div class="form-group row justify-content-between pl-3 pr-3">
+                        <div class="form-group col-sm-4 col-12 pr-sm-4 pr-0 pl-0">
+                            <span for="text_1" class="mb-2 d-inline-block">text</span>
+                            <input type="text" class="form-control" name="text_1" id="text_1"  >
+                        </div>
+                        <div class="form-group col-sm-4 col-12 pr-sm-4 pl-sm-4 pr-0 pl-0">
+                            <span for="text_2" class="mb-2 d-inline-block ">text</span>
+                            <input type="text" class="form-control" name="text_2" id="text_2"  >
+                        </div>
+                        <div class="form-group col-sm-4 col-12 pr-0 pl-sm-4 pl-0">
+                            <span for="text_3" class="mb-2 d-inline-block ">text</span>
+                            <input type="text" class="form-control" name="text_3" id="text_3"  >
+                        </div>
+                    </div>
+                    <a href="#" class="btn text-white shadow "><span class="mdi mdi-arrow-left pr-3"></span>Назад</a>
+                    <a href="#" class="btn text-white float-right shadow ">Далее<span class="mdi mdi-arrow-right pl-3"></span></a>
+                </form>
+            </div>
+        
+        </div> <!--row-->
+        
+    </div> <!--container-->
+</section>
