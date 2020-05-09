55f3b194c6eeb37eae5d8d34920f240aa6df39db
Andrew489 <andrew48914@gmail.com>
2020-05-09 15:03:41 +0500

    gdego

--- blocks/add_review.php
+++ blocks/add_review.php
@@ -5,25 +5,26 @@
         </div>
     </div>
     <div class="row mb-4">
-        <div class="col-6">
+        <div class="col-sm-6">
             <span class="text-100">Имя</span>
-            <input type="text" placeholder="Представьтесь, пожалуйста" name="name">
+            <input type="text" placeholder="Представьтесь, пожалуйста" name="name" class="w-100">
         </div>
-        <div class="col-6">
+        <div class="col-sm-6">
             <span class="text-100">Email</span>
-            <input type="text" placeholder="Ваш контактный email" name="email">
+            <input type="text" placeholder="Ваш контактный email" name="email" class="w-100">
         </div>
     </div>
     <div class="row mb-4">
         <div class="col-12">
-            <input type="text" placeholder="Всем очень рекомендую" name="">
+            <input type="text" placeholder="Всем очень рекомендую" name="" class="w-100">
         </div>
     </div>
     <div class="row">
         <div class="col-12">
-            <textarea></textarea>
+            <textarea class="w-100" name="text"></textarea>
         </div>
-    </div><div class="row">
+    </div>
+    <div class="row">
         <div class="col-12">
 
         </div>
