7e91c5eafa807c3119e7d378240a2972371c3212
jawsspot <65167112+jawsspot@users.noreply.github.com>
2020-05-14 23:29:13 +0500

    v

---
+++ page/ui_kit.php
@@ -0,0 +1,116 @@
+<div class="container ui_kit">
+
+	<nav class="font-weight-bold">
+		<ul class="list-inline row  mt-3 ">
+			<li class="col-2"><a class="text-dark" href="#">Default</a></li>
+			<li class="col-2"><a class="text-dark" href="#">Focus</a></li>
+			<li class="col-2"><a class="text-dark" href="#">Done</a></li>
+			<li class="col-2"><a class="text-dark" href="#">Error</a></li>
+			<li class="col-2"><a class="text-dark" href="#">Disabled</a></li>
+		</ul>
+	</nav>
+
+	<div class="row mt-5">
+		<div class="form-check col-2">
+			<input class="form-check-input" type="checkbox" name="exampleRadios" id="exampleCheckbox" value="option1" checked>
+			<label class="form-check-label" type="checkbox" for="exampleCheckbox">
+				<span class="">Checkbox 1: false</span>
+			</label>
+		</div>
+
+		<div class="col-2"></div>
+		
+		<div class="form-check col-2">
+			<input class="form-check-input" type="checkbox" name="exampleRadios" id="exampleCheckbox2" value="option2">
+			<label class="form-check-label " type="checkbox" for="exampleCheckbox2">
+				<span class="">Checkbox 2: true</span>
+			</label>
+		</div>
+
+		<div class="col-2"></div>
+
+		<div class="col-2"></div>
+	</div>
+
+	<div class="row mt-5">
+		<div class="form-check col-2">
+			<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+			<label class="form-check-label " type="radio" for="exampleRadios1">
+				<span class="">radio 1</span>
+			</label>
+		</div>
+
+		<div class="col-2"></div>
+
+		<div class="form-check col-2">
+			<input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+			<label class="form-check-label " type="radio" for="exampleRadios2">
+				<span class="">radio 2</span>
+			</label>
+		</div>
+
+		<div class="col-2"></div>
+
+	</div>
+
+<div class="row">
+	<div class="col-2">	<!-- Rounded switch -->
+		<label class="switch mb-0 mt-5">
+			<input type="checkbox">
+			<span class="slider round"></span>
+		</label>
+		<span class="ml-2">Switch 2: False</span>
+	</div>
+
+	<div class="col-2"></div>
+
+	<div class="col-2">
+		<label class="switch mb-0 mt-5">
+			<input type="checkbox">
+			<span class="slider round"></span>
+		</label>
+		<span class="ml-2">Switch 1: False</span>
+	</div>
+
+	<div class="col-2"></div>
+
+	<div class="col-2">
+		<label class="switch mb-0 mt-5">
+			<input type="checkbox">
+			<span class="slider round"></span>
+		</label>
+		<span class="ml-2">Switch 1: False</span>
+	</div>
+</div>
+
+<div class="row mt-5">
+	
+	<form class="col-2 default" action="">
+		<input class="form-control pl-4" type="text" placeholder="Enter your message" name="text" aria-label="Search">
+		<span class="mdi mdi-magnify search-icon"></span>
+	</form>
+	
+	<form class="col-2 focus" action="">
+		<input class="form-control pl-4" type="text" placeholder="Enter your message" name="text" aria-label="Search">
+		<span class="mdi mdi-magnify search-icon"></span>
+	</form>
+	
+	<form class="col-2 done" action="">
+		<input class="form-control pl-4" type="text" placeholder="Enter your message" name="text" aria-label="Search">
+		<span class="mdi mdi-magnify search-icon"></span>
+	</form>
+	
+	<form class="col-2 error" action="">
+		<input class="form-control pl-4" type="text" placeholder="Enter your message" name="text" aria-label="Search">
+		<span class="mdi mdi-magnify search-icon"></span>
+	</form>
+	
+	<form class="col-2 disabled" action="">
+		<input class="form-control pl-4" type="text" placeholder="Enter your message" name="text" aria-label="Search">
+		<span class="mdi mdi-magnify search-icon"></span>
+	</form>
+	
+</div>
+
+
+	
